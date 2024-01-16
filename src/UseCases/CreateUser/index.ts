4f026a6b6171536b8b5fb8afcae9f0949247d798
Tarcísio Júnior <tarcisio.junior1406@gmail.com>
2024-01-16 20:41:39 -0300

    Update index.ts
    Remove seringa connection

--- src/UseCases/CreateUser/index.ts
+++ src/UseCases/CreateUser/index.ts
@@ -1,4 +1,4 @@
-import { MailtrapMailProvider } from "../../providers/implementations/MailtrapMailProvider";
+seimport { MailtrapMailProvider } from "../../providers/implementations/MailtrapMailProvider";
 import { MongoUserRepository } from "../../repositories/implementations/MongoUserRepository";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 import { CreateUserController } from "./CreateUserController";
@@ -6,7 +6,6 @@ import { CreateUserController } from "./CreateUserController";
 const mongoUserRepository = new MongoUserRepository();
 const mailtrapMailProvider = new MailtrapMailProvider();
 
-mongoose.connect('mongodb+srv://junior:M7OopgRPAlax0NKG@cluster0.u5l8x.mongodb.net/?retryWrites=true&w=majority')
 
 const createUserUseCase = new CreateUserUseCase(
     mongoUserRepository,
